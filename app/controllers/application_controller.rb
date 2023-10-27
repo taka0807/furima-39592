d41fba2b9d7fd2034d4404685af1a1a55afca668
taka0807 <takafumi0807mail-0807@yahoo.co.jp>
2023-10-27 21:40:09 +0900

    メモ（環境変数）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,3 +10,21 @@ class ApplicationController < ActionController::Base
     end
   end
 end
+
+# 環境変数はどのパソコンでも一つのものに統一されている。
+# アプリによって追加したい場合は、ENV['BASIC_of_USER']ENV['BASIC_of_PASSWORD']にて追加。
+# 変数として認識させる。(vc上で特定させない為に)
+# BASIC_of_USER=(vim ~/.profile)で入力したuser
+# BASIC_of_PASSWORD=(vim ~/.profile)で入力したpassword
+
+# vim ~/.profile
+# →export BASIC_AUTH_USER="admin"
+# export BASIC_AUTH_PASSWORD="2222"
+# export BASIC_of_USER="yuuri"
+# export BASIC_of_PASSWORD="39592"
+# ターミナル上では大文字で記載。Esc→:wq→enter
+# 追加変更した後は、source ~/.profileをターミナルで入力、enterで保存完了。
+# その後、renderのEnvironmentにて、addで追加後、追加したkeyとvalueを入力後、save。
+# →logを見て、liveが出た後、urlを確認。
+# 一度、ログイン（userとパスワード)した場合はシークレットモード→上のurlにlocallhostで再入場。
+
