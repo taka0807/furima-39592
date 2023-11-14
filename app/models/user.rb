dda37142ba08bcc9422eee2327a0fccad736c871
taka0807 <takafumi0807mail-0807@yahoo.co.jp>
2023-11-14 14:37:20 +0900

    修正（途中）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          validates :nickname, presence: true
-         validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[0-9]).{6,}\z/i, message: 'は半角英数字混合で入力してください' }
-         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/, message: 'は全角文字で入力してください' }
-         validates :password_confirmation, presence: true
-         validates :password, confirmation: true # パスワードとパスワード（確認）が一致していることを確認
-         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/, message: 'は全角文字で入力してください' }
+         validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は半角英数字混合で入力してください' }
+         validates :last_name, presence: true, format: { with: \A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字で入力してください' }
+         validates :first_name, presence: true, format: { with: \A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字で入力してください' }
          validates :furigana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カナ文字で入力してください' }
          validates :furigana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カナ文字で入力してください' }
          validates :birthday, presence: true
@@ -16,3 +14,5 @@ end
 
 
 # 正規表現
+#  validates :password_confirmation, presence: true
+        #  validates :password, confirmation: true # パスワードとパスワード（確認）が一致していることを確認
