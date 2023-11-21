c7178844be15a8206ce051eb262c5197e0ce7c44
taka0807 <takafumi0807mail-0807@yahoo.co.jp>
2023-11-21 16:09:27 +0900

    修正完了分

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -9,12 +9,12 @@ FactoryBot.define do
   factory :item do
     productname { Faker::Commerce.product_name }
     explanation { Faker::Lorem.paragraph }
-    category_id { 1 } # もしくは category_id: 1 (適切なIDを指定)
-    product_condition_id { 1 } # 適切なIDを指定
-    pay_of_shipping_id { 1 } # 適切なIDを指定
-    region_of_origin_id { 1 } # 適切なIDを指定
-    number_of_days_until_shipping_id { 1 } # 適切なIDを指定
-    price { Faker::Commerce.price(range: 1000..10000) }
+    category_id { rand(2..11) } # もしくは category_id: 1 (適切なIDを指定)
+    product_condition_id { rand(2..7) } # 適切なIDを指定
+    pay_of_shipping_id { rand(2..3) } # 適切なIDを指定
+    region_of_origin_id { rand(2..48) } # 適切なIDを指定
+    number_of_days_until_shipping_id { rand(2..4) } # 適切なIDを指定
+    price { rand(300..9999999) }
     association :user
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
