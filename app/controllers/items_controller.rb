6b23143e0a9d2bca6f0d4923b17a679d19201e5c
taka0807 <takafumi0807mail-0807@yahoo.co.jp>
2023-11-24 17:41:00 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-  skip_before_action :authenticate_user!, only: :index
   def index
     @items = Item.order(created_at: :desc)
   end
