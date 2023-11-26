9efc0794fbd949903caa3425228281b3f68f1679
taka0807 <takafumi0807mail-0807@yahoo.co.jp>
2023-11-26 16:25:57 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,13 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+
   def index
     @items = Item.order(created_at: :desc)
   end
 
   def show
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
   end
 
 def new
@@ -14,7 +16,7 @@ end
 
 
 def edit
-  @item = Item.find(params[:id])
+  # @item = Item.find(params[:id])
 
   if user_signed_in? && current_user != @item.user
     redirect_to root_path, alert: 'You are not authorized to edit this item.'
@@ -23,7 +25,7 @@ end
 
 
 def update
-  @item = Item.find(params[:id])
+  # @item = Item.find(params[:id])
   if @item.update(item_params)
     redirect_to item_path(@item)
   else
@@ -39,6 +41,10 @@ def create
   else
     render :new, status: :unprocessable_entity
   end
+end
+
+def set_item
+  @item = Item.find(params[:id])
 end
 
 
