976bf36b5f2aacbfbdec8447a9e0bd76456aa423
taka0807 <takafumi0807mail-0807@yahoo.co.jp>
2023-12-10 18:52:41 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,10 +12,10 @@ has_one_attached :image
 validates :image, presence: true
 	validates :productname, presence: true
 	validates :explanation, presence: true
-	validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
-	validates :product_condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
-	validates :pay_of_shipping_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
-	validates :region_of_origin_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
-	validates :number_of_days_until_shipping_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+	validates :category_id, presence: true, numericality: { other_than: 1 , message: "Category can't be blank" }
+	validates :product_condition_id, presence: true, numericality: { other_than: 1 , message: "Product condition can't be blank" }
+	validates :pay_of_shipping_id, presence: true, numericality: { other_than: 1 , message: "Pay of shipping can't be blank" }
+	validates :region_of_origin_id, presence: true, numericality: { other_than: 1 , message: "Region of origin can't be blank" }
+	validates :number_of_days_until_shipping_id, presence: true, numericality: { other_than: 1 , message: "Number of days until shipping can't be blank" }
 	validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end
