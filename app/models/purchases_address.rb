bb9e78431ed8f8b3c382e4de71e02504624995d6
taka0807 <takafumi0807mail-0807@yahoo.co.jp>
2023-12-10 19:38:26 +0900

    修正

--- app/models/purchases_address.rb
+++ app/models/purchases_address.rb
@@ -4,7 +4,6 @@ class PurchasesAddress
 
 	with_options presence: true do
     validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: "は「3桁ハイフン4桁」の形式で入力してください" }
-    validates :region_of_origin_id, numericality: { other_than: 1 , message: "can't be blank" }
     validates :municipalities
     validates :street_address
     validates :telephone_number, format: { with: /\A\d{10,11}\z/, message: "は10桁以上11桁以内の半角数値で入力してください" }
@@ -12,6 +11,8 @@ class PurchasesAddress
     validates :user_id
     validates :item_id
   end
+
+  validates :region_of_origin_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     # 購入情報を保存し、変数purchaseに代入する
